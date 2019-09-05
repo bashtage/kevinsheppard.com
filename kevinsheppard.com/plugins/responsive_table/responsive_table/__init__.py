dbbb6c86ba9ae0fd9cddbba02c4633e90165a3f9
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2019-09-05 18:03:41 +0100

    Add a plugin to fix tables

--- kevinsheppard.com/plugins/responsive_table/responsive_table/__init__.py
+++ kevinsheppard.com/plugins/responsive_table/responsive_table/__init__.py
@@ -5,6 +5,7 @@ import glob
 import os
 
 from bs4 import BeautifulSoup
+from nikola import utils
 from nikola.plugin_categories import LateTask
 
 
@@ -13,20 +14,29 @@ class ResponsiveTable(LateTask):
 
     name = "responsive_table"
 
-    @staticmethod
-    def fix_tables(file_name: str):
-        with open(file_name) as html:
-            soup = BeautifulSoup(html.read())
-        tables = soup.findChildren('table', recursive=True)
-        for table in tables:
-            print('Found a table in ' + file_name)
-            table.wraps(soup.new_tag('div', **{'class': 'table-responsive'}))
-        with open(file_name, 'w') as html:
-            html.write(str(soup))
+    def fix_tables(self):
+        html_files = glob.glob(
+            os.path.join(self.kw['output_folder'], '**', '*.html'),
+            recursive=True)
+        for file_name in html_files:
+            with open(file_name) as html:
+                soup = BeautifulSoup(html.read(), features='lxml')
+            tables = soup.findChildren('table', recursive=True)
+            for table in tables:
+                utils.LOGGER.info('Found a table in ' + file_name)
+                if (table.parent.name == 'div'
+                        and 'class' in table.parent.attrs
+                        and table.parent.attrs['class'] == ['table-responsive']):
+                    utils.LOGGER.info('Skipping table!!!')
+                    continue
+                table.wrap(
+                    soup.new_tag('div', **{'class': 'table-responsive'}))
+            with open(file_name, 'w') as html:
+                html.write(str(soup))
 
     def gen_tasks(self):
         """Add div to make tables responsive."""
-        kw = {
+        self.kw = {
             "base_url": self.site.config["BASE_URL"],
             "site_url": self.site.config["SITE_URL"],
             "output_folder": self.site.config["OUTPUT_FOLDER"],
@@ -34,12 +44,8 @@ class ResponsiveTable(LateTask):
             "robots_exclusions": self.site.config["ROBOTS_EXCLUSIONS"],
             "filters": self.site.config["FILTERS"],
         }
-        html_files = glob.glob(
-            os.path.join(kw['output_folder'], '**', '*.html'),
-            recursive=True)
 
         yield self.group_task()
-        for html_file in html_files:
-            yield {"basename": str(self.name),
-                   'name': html_file,
-                   'actions': [(self.fix_tables, (html_file,))]}
+        yield {"basename": str(self.name),
+               'name': 'Make tables responsive',
+               'actions': [self.fix_tables]}
