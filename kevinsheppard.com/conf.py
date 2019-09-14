5ca9687702ea35b63113677a2a474df5aba936b7
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2019-09-15 00:09:32 +0100

    Enable comments

--- kevinsheppard.com/conf.py
+++ kevinsheppard.com/conf.py
@@ -990,12 +990,12 @@ RSS_COPYRIGHT_FORMATS = CONTENT_FOOTER_FORMATS
 # systems.  The following comment systems are supported by Nikola:
 #   disqus, facebook, intensedebate, isso, muut, commento
 # You can leave this option blank to disable comments.
-COMMENT_SYSTEM = ""
+COMMENT_SYSTEM = "disqus"
 # And you also need to add your COMMENT_SYSTEM_ID which
 # depends on what comment system you use. The default is
 # "nikolademo" which is a test account for Disqus. More information
 # is in the manual.
-COMMENT_SYSTEM_ID = ""
+COMMENT_SYSTEM_ID = "kevin-sheppard"
 
 # Create index.html for page folders?
 # WARNING: if a page would conflict with the index file (usually
@@ -1003,9 +1003,9 @@ COMMENT_SYSTEM_ID = ""
 #          will not be generated for that directory.
 # PAGE_INDEX = False
 # Enable comments on pages (i.e. not posts)?
-# COMMENTS_IN_PAGES = False
+COMMENTS_IN_PAGES = False
 # Enable comments on picture gallery pages?
-# COMMENTS_IN_GALLERIES = False
+COMMENTS_IN_GALLERIES = False
 
 # What file should be used for directory indexes?
 # Defaults to index.html
