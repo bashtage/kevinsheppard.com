0b9f9f65dcbd7412661d882c775b9eb0f95d1586
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2022-01-06 18:52:21 +0000

    Add clean command

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -4,7 +4,7 @@ echo "Set git email and name"
 git config user.email "kevin.k.sheppard@gmail.com"
 git config user.name "Kevin Sheppard"
 echo "Checkout pages"
-git clean -xfd
+git clean ./site/images -xfd
 git checkout gh-pages
 echo "Copy docs to root"
 echo cp -r ${PWD}/site/output/* ${PWD}/
